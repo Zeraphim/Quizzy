4a3d9dbf4c98a0c3519a8a61c647e0c4ebf53902
Zeraphim <diamantejc87@gmail.com>
2024-08-12 03:04:14 +0800

    init, installed tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-screen w-screen flex items-center justify-center bg-teal-400">
+        GemiTravel
+      </div>
+    </>
+  )
+}
+
+export default App
