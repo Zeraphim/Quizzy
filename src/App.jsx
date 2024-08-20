f6f32973337a65f82c63300e3989015b0d840950
Zeraphim <diamantejc87@gmail.com>
2024-08-21 06:32:58 +0800

    Main dashboard layout, added tailwindcss cdn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import Main from "./components/MainDashboard"
+
 function App() {
 
   return (
     <>
-      <div className="h-screen w-screen flex items-center justify-center bg-teal-400">
-        GemiTravel
+      <div className="h-screen w-screen flex items-center justify-center bg-slate-900">
+        <Main />
       </div>
     </>
   )
---
+++ src/components/MainDashboard.jsx
@@ -0,0 +1,21 @@
+
+function MainDashboard() {
+  return (
+    <div className="h-screen w-screen text-white text-4xl font-bold flex items-center justify-center">
+      <div className="grid grid-rows-4 grid-flow-col gap-4">
+        {/* Question Banks */}
+        <div className="col-span-2 row-span-2 flex items-center justify-center w-[35vw] h-[40vh] backdrop-blur-sm gap-6 bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">Question Banks</div>
+        {/* Math Banks */}
+        <div className="col-span-2 row-span-2 flex items-center justify-center w-[35vw] h-[40vh] bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">Math Banks</div>
+        {/* Start now */}
+        <div className="row-span-4 flex items-center justify-center w-[20vw] bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">Study Now</div>
+        {/* Profile */}
+        <div className="col-span-2 row-span-2 flex items-center justify-center w-[35vw] h-[40vh] bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">Profile</div>
+        {/* Analytics */}
+        <div className="col-span-2 row-span-2 flex items-center justify-center w-[35vw] h-[40vh] bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">Analytics</div>
+      </div>
+    </div>
+  )
+}
+
+export default MainDashboard
