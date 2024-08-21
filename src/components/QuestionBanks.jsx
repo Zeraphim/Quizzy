8a6536b4a73be8c0b04a2d3c313e5f7c85949653
Zeraphim <diamantejc87@gmail.com>
2024-08-21 08:28:31 +0800

    Installed DaisyUI, questions from question banks styling

--- src/components/QuestionBanks.jsx
+++ src/components/QuestionBanks.jsx
@@ -20,7 +20,7 @@ function QuestionBanks() {
                 <div className="h-[90%] max-h-[90%] w-full flex flex-col items-center justify-start gap-4">
 
                     {Object.keys(question_banks_data).map((quesBankTitle) => (
-                        <div key={quesBankTitle} className={`w-full h-[5vh]  ${chosenQuestionBank === quesBankTitle ? "bg-violet-900" : "bg-slate-900"} border-slate-800 border-2 hover:bg-slate-950 transition text-white font-semibold text-2xl  flex items-center justify-center`} onClick={() => setChosenQuestionBank(quesBankTitle)}>
+                        <div key={quesBankTitle} className={`w-full h-[5vh]  ${chosenQuestionBank === quesBankTitle ? "bg-violet-900" : "bg-slate-900 hover:bg-slate-950"} border-slate-800 border-2 transition text-white font-semibold text-2xl  flex items-center justify-center`} onClick={() => setChosenQuestionBank(quesBankTitle)}>
                             {quesBankTitle}
                         </div>
                     ))}
@@ -28,7 +28,32 @@ function QuestionBanks() {
                 </div>
                 
             </div>
-            <div className="h-full w-[60vw] bg-blue-300">2</div>
+            <div className="h-full w-[60vw] flex flex-col items-center justify-start gap-6 text-white font-bold text-xl">
+                
+                {chosenQuestionBank && question_banks_data[chosenQuestionBank].map((question, index) => (
+                // <div key={index} className="w-full h-28 flex items-center justify-center bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white">
+                //     {question.Question}
+                // </div>
+
+                <div key={index} tabIndex="0" className="collapse collapse-arrow border-base-300 bg-base-200 border">
+                    <div className="collapse-title">
+                        <div className="text-sm text-violet-400">
+                            {question.Type}
+                        </div>
+                        <div className="text-xl font-medium">
+                            {question.Question}
+                        </div>
+
+                    </div>
+                    <div className="collapse-content">
+                        <p>{question.Answer}</p>
+                    </div>
+                </div>
+                ))}
+
+
+
+            </div>
             <div className="h-full w-[20vw] bg-orange-300">3</div>
         </div>
     )
