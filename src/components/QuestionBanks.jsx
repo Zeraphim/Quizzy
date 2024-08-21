2ff81fe1ca189acc3cdf84a3974abdd8cfeb0bb5
Zeraphim <diamantejc87@gmail.com>
2024-08-21 09:06:10 +0800

    Question Banks component minor changes to styling

--- src/components/QuestionBanks.jsx
+++ src/components/QuestionBanks.jsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 
 function QuestionBanks() {
 
-    const [chosenQuestionBank, setChosenQuestionBank] = useState("")
+    const firstQuestionBank = Object.keys(question_banks_data)[0];
+    const [chosenQuestionBank, setChosenQuestionBank] = useState(firstQuestionBank);
 
     useEffect(() => {
         console.log(chosenQuestionBank)
@@ -14,7 +15,7 @@ function QuestionBanks() {
     return (
         <div className="h-screen max-h-screen w-screen flex flex-row items-center justify-center p-9 gap-6">
             {/* Question Banks List Container */}
-            <div className="h-full w-[20vw] p-8 gap-6 flex flex-col items-center justify-start bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">
+            <div className="h-full w-[20vw] py-8 px-6 gap-6 flex flex-col items-center justify-start bg-white rounded-3xl overflow-hidden bg-opacity-10 border-2 border-opacity-[4%] border-white z-40">
 
                 <h1 className="text-white font-bold text-3xl">Question Banks</h1>
 
@@ -40,6 +41,7 @@ function QuestionBanks() {
                 // </div>
 
                 <div key={index} tabIndex="0" className="collapse collapse-arrow border-base-300 bg-base-200 border">
+                    <input type="checkbox" />
                     <div className="collapse-title">
                         <div className="text-sm text-violet-400">
                             {question.Type}
@@ -60,15 +62,15 @@ function QuestionBanks() {
             {/* Action Buttons Container */}
             <div className="h-full w-[20vw] text-white font-bold text-xl flex flex-col items-center justify-start px-6 gap-6">
                 <div className="w-full h-20 rounded-lg flex items-center justify-center bg-slate-800 hover:bg-violet-800 transition">
+                    <Link to="/" className="w-full h-full flex items-center justify-center">
+                        Back
+                    </Link>
+                </div>
+                <div className="w-full h-20 rounded-lg flex items-center justify-center bg-slate-800 hover:bg-violet-800 transition">
                     Add Questions
                 </div>
                 <div className="w-full h-20 rounded-lg flex items-center justify-center bg-slate-800 hover:bg-violet-800 transition">
                     Study Now
-                </div>
-                <div className="w-full h-20 rounded-lg flex items-center justify-center bg-slate-800 hover:bg-violet-800 transition">
-                    <Link to="/" className="w-full h-full flex items-center justify-center">
-                        Back
-                    </Link>
                 </div>
             </div>
         </div>
