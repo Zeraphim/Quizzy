877487a8352e25853f5039d8155200b0fccdbfa7
Zeraphim <diamantejc87@gmail.com>
2024-09-04 18:16:08 +0800

    Dark mode not working on vercel deployment attempt fix 3

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -29,7 +29,7 @@ function MainLayout({toggleDarkMode, darkMode}) {
                     <path
                       d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
                   </svg>
-                <input type="checkbox" value="synthwave" className="toggle theme-controller" onClick={toggleDarkMode} checked={darkMode}/>
+                <input type="checkbox" value="dark" className="toggle theme-controller" onClick={toggleDarkMode} checked={darkMode}/>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="20"
